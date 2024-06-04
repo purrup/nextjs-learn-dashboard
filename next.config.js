d6c2f203cea138719b3a127b437958afd0253ddb
TSUNG-MING TSAI <purrup27@gmail.com>
2024-06-04 06:07:38 +0000

    chore: upgrade next version to solve cors issue when developing in codespace environment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["process.ENV.HOST", "localhost:3000"]
+    }
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
